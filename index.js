e2047955298c58a640346af2050573b81a1841e0
Raddames <raddamestonui48@gmail.com>
2024-03-24 19:10:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+ // Create a variable called multiply set to an equation
+//  that will multiply the variables num1 and num2; 
+//  the result of the multiplication should be 62.
+let num1 = 31;
+let num2 =2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+//Create a variable called random that will generate a random integer greater than 0.
+let random = Math.round(Math.random() * 10) 
+console.log(random);
+
+// Create a variable called mod set to an equation 
+//  will calculate the remainder of dividing variable num3 by num4; 
+// the remainder should be 4.
+
+let num3 = 16;
+let num4 = 12;
+let mod = num3 % num4;
+
+console.log(mod);
+
+let numm = [17, 15, 11, 20 , 18, 1, 7]
+const max = Math.max(17, 15, 11, 20 , 18, 1, 7)
+console.log(max);
